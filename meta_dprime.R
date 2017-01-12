b41e03078d378dbfeefe15c1f4ebc0b61c5c93cb
Magdalena <majducha@users.noreply.github.com>
2017-01-12 19:20:21 +0100

    now also outputs one data frame with all plots

--- meta_dprime.R
+++ meta_dprime.R
@@ -11,6 +11,7 @@ rm(list=ls())
 
 # Import Packages 
 source("essential_functions_libraries.R")
+source("meta_dprime_functions.R")
 
 # Load my.data (if it is not already present)
 if (!exists("my.data")){
@@ -19,10 +20,6 @@ if (!exists("my.data")){
 }
 
 ####################################################################
-
-##############
-# SOCIAL INFO
-##############
 
 ### PRE-ALLOCATION
 # load participants' IDs
@@ -40,11 +37,9 @@ m.d <- matrix(NA, ncol = 3, nrow = length(pID))
 colnames(m.d)<-names
 
 # graphs
-fig.meta<- list()
-fig.d<-list()
-fig.d_meta <-list()
 fig.p <- list()
 
+#################################################################
 
 ### CALCULALTE META-D' and D':
 
@@ -63,7 +58,7 @@ for (iSI in 1:3){
 }
 
 
-### PREPARE FOR PLOTTING
+### CONVERT THE MATRIX INTO A DATA FRAME ANd CALCULATE D'DIFF
 
 #convert matrices into data.frames (for ggplot, and easier handling)
 m.meta <- data.frame(m.meta)
@@ -77,13 +72,7 @@ m.meta$participant<-pID
 m.d$participant<-pID
 m.diff$participant<-pID
 
-# save the matrices
-save(m.meta,file='m.meta.RData') 
-save(m.d,file='m.d.RData') 
-save(m.diff,file='m.diff.RData') 
-
-
-### PLOT IT
+### CREATE THE PLOTS
 
 # PLOT by participant
 # change the structure of the d.frame so that I can plot multiple columns
@@ -91,58 +80,69 @@ m.meta.p<-melt(m.meta,id.vars="participant")
 m.d.p <-melt(m.d,id.vars="participant")
 m.diff.p <-melt(m.diff,id.vars="participant")
 
-# define the form of the graph
-p.meta = ggplot(aes(x=variable, y=value),data=m.meta.p) + 
+# META-D'
+p.meta = ggplot(aes(x=variable, y=value, fill=variable),data=m.meta.p) + 
   geom_bar(stat = "identity") + 
   xlab("condition") + ylab("meta-d") + 
-  #scale_fill_manual(values=c("#F5A503","#56D9CD", "#3AA1BF")) +
+  scale_fill_manual(values=c("#F5A503","#56D9CD", "#3AA1BF")) +
   geom_abline(intercept = 0, slope = 0)
 
-p.d = ggplot(aes(x=variable, y=value),data=m.d.p) + 
+f.meta <- group_by(m.meta.p,participant) %>%
+  do(meta = p.meta %+% ., 
+     meta_F = p.meta + facet_wrap(~ participant))
+
+# D'
+p.d = ggplot(aes(x=variable, y=value, fill=variable),data=m.d.p) + 
   geom_bar(stat = "identity") + 
   xlab("condition") + ylab("dPrime") + 
-  #scale_fill_manual(values=c("#F5A503","#56D9CD", "#3AA1BF")) +
+  scale_fill_manual(values=c("#F5A503","#56D9CD", "#3AA1BF")) +
   geom_abline(intercept = 0, slope = 0)
 
-p.diff = ggplot(aes(x=variable, y=value),data=m.diff.p) + 
+f.d <- group_by(m.d.p,participant) %>%
+  do(d = p.d %+% .,
+     d_F = p.d + facet_wrap(~ participant))
+
+# D'-DIFF - metacognitive efficiency (meta-d' - d')
+p.diff = ggplot(aes(x=variable, y=value, fill=variable),data=m.diff.p) + 
   geom_bar(stat = "identity") + 
   xlab("condition") + ylab("metaD-dPrime")+ 
-  #scale_fill_manual(values=c("#F5A503","#56D9CD", "#3AA1BF")) +
+  scale_fill_manual(values=c("#F5A503","#56D9CD", "#3AA1BF")) +
   geom_abline(intercept = 0, slope = 0)
 
-# do the graph for all participants and save it into a list under the heading plots, also do a facet-wrap graph (plots2)
-fig.meta.p <- group_by(m.meta.p,participant) %>%
-  do(plots = p.meta %+% ., plot_F = p.meta + facet_wrap(~ participant))
+f.diff <- group_by(m.diff.p,participant) %>%
+  do(diff = p.diff %+% ., 
+     diff_F = p.diff + facet_wrap(~ participant))
 
-fig.d.p <- group_by(m.d.p,participant) %>%
-  do(plots = p.d %+% .,plot_F = p.d + facet_wrap(~ participant))
+### PUT ALL PLOTS INTO A SINGLE TABLE
 
-fig.diff.p <- group_by(m.diff.p,participant) %>%
-  do(plots = p.diff %+% ., plot_F = p.diff + facet_wrap(~ participant))
+# create a list with all data.frames to be merged
+f.all <- list(f.meta,f.d,f.diff)
+
+# use reduce to call function merge (merges two data.frames)
+meta_plots <- Reduce(function(...) merge(...,by = "participant", all = TRUE), f.all)
+
+# save it
+save(meta_plots,file='meta_plots.RData')
+
+### SAVE THE DATA FILES
+m.meta$type<-"meta"
+m.d$type<-"dprime"
+m.diff$type<-"diff"
+
+m.combined <-rbind(m.meta,m.d,m.diff)
+
+save(m.combined,file='meta_values.RData') 
 
 # Put meta and dPrime together 
 
-for(i in 1:length(unique(m.meta.p$participant))){
+#for(i in 1:length(unique(m.meta.p$participant))){
   # D', metaD' and D'diff in one graph
   #fig.p[[i]]<- grid.arrange(fig.meta.p$plots[[i]],fig.d.p$plots[[i]], fig.diff.p$plots[[i]],ncol=1, nrow =3, top=as.character(unique(m.meta.p$participant)[i]))
 
   # D', metaD' and D'diff in one graph
-  fig.p[[i]]<- grid.arrange(fig.meta.p$plots[[i]],fig.d.p$plots[[i]],ncol=1, nrow =2, top=as.character(unique(m.meta.p$participant)[i]))
-}
+ # fig.p[[i]]<- grid.arrange(fig.meta.p$plots[[i]],fig.d.p$plots[[i]],ncol=1, nrow =2, top=as.character(unique(m.meta.p$participant)[i]))
+#}
 
 # Plot D' and metaD' together by participant
-dev.new()
-grid.arrange(fig.p[[1]],fig.p[[2]],fig.p[[3]],fig.p[[4]],fig.p[[5]],fig.p[[6]],fig.p[[7]], ncol=4, nrow=2)
-
-#Plot D' by participant
-dev.new()
-fig.d.p$plot_F[1]
-
-#Plot metaD' by participant
-dev.new()
-fig.meta.p$plot_F[1]
-
-#Plot D'diff by participant
-dev.new()
-fig.diff.p$plot_F[1]
+#grid.arrange(fig.p[[1]],fig.p[[2]],fig.p[[3]],fig.p[[4]],fig.p[[5]],fig.p[[6]],fig.p[[7]], ncol=4, nrow=2)
 
